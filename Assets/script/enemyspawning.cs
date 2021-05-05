652929dbf786befb0db794e88654fc5c89027d0d
Finleyw <511346@chrome.escg.ac.uk>
2021-05-05 23:01:38 +0100

    bloop

---
+++ Assets/script/enemyspawning.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyspawning : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform spawn1;
+    
+    public Transform spawn3;
+    
+    void Update()
+    {
+        /*if()
+        {
+            Instantiate(enemy,spawn1.position,spawn1.rotation);
+            Instantiate(enemy,spawn2.position,spawn2.rotation);
+            Instantiate(enemy,spawn3.position,spawn3.rotation);
+        }*/
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(enemy,spawn1.position,spawn1.rotation);
+           
+            Instantiate(enemy,spawn3.position,spawn3.rotation); 
+        }
+    }
+}
