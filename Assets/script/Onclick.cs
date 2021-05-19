f953f1bb466ea0c918e6e72139895dfa96577cf1
Finleyw <72386519+Finleyw@users.noreply.github.com>
2021-05-19 16:27:58 +0100

    AAAAAA end it now!!

---
+++ Assets/script/Onclick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class Onclick : MonoBehaviour, ISelectHandler , IPointerEnterHandler
+{
+    public AudioManager sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+    
+        print("selected");
+        sound.Play("click");
+    }
+}
